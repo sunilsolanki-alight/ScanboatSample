3f3dbb665c26ef016af164b0bf1838fa42128f49
sunilsolanki-alight <119950546+sunilsolanki-alight@users.noreply.github.com>
2025-03-05 16:32:32 +0530

    first commit

---
+++ ScanboatSample/Platforms/Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Firebase.Perf;
+namespace ScanboatSample
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            FirebasePerformance.Instance.PerformanceCollectionEnabled = true;
+            FirebasePerformance.Instance.SetPerformanceCollectionEnabled((Java.Lang.Boolean)true);
+
+
+        }
+    }
+}
