bc1f91c68d3b463ee91bdd9941619071ba23050c
sunilsolanki-alight <119950546+sunilsolanki-alight@users.noreply.github.com>
2025-03-07 11:35:05 +0530

    removed pref refrence

--- ScanboatSample/Platforms/Android/MainActivity.cs
+++ ScanboatSample/Platforms/Android/MainActivity.cs
@@ -1,7 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Firebase.Perf;
+//using Firebase.Perf;
 namespace ScanboatSample
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
@@ -10,10 +10,8 @@ namespace ScanboatSample
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            FirebasePerformance.Instance.PerformanceCollectionEnabled = true;
-            FirebasePerformance.Instance.SetPerformanceCollectionEnabled((Java.Lang.Boolean)true);
-
-
+            //FirebasePerformance.Instance.PerformanceCollectionEnabled = true;
+            //FirebasePerformance.Instance.SetPerformanceCollectionEnabled((Java.Lang.Boolean)true);
         }
     }
 }
